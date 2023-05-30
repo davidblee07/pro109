4c9510c71f073b7db2f64b77e67ca6bd8668ae59
davidblee07 <93348233+davidblee07@users.noreply.github.com>
2023-05-30 16:58:26 -0300

    Update take_screenshot.py

--- pro109/take_screenshot.py
+++ pro109/take_screenshot.py
@@ -44,13 +44,13 @@ while True:
             print(finger_fold_status)
 
              #verificando se todos os dedos estão dobrados
-            if all(finger_fold_status):
+        if all(finger_fold_status):
                 
                 # ESCREVA O CÓDIGO AQUI  
              image = pyautogui.screenshot()
              image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
              cv2.imwrite("in_memory_to_disk.png", image)
-             pyautogui.screenshot("straight_to_disks.png")
+             pyautogui.screenshot("straight_to_disk.png")
              image = cv2.imread("straight_to_disk.png")
              cv2.imshow("Captura de tela", imutils.resize(image, width=600))
 
