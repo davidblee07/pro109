4b03856ace0ea33b8943ddc61972f65deeb2bc80
davidblee07 <93348233+davidblee07@users.noreply.github.com>
2023-07-04 18:51:05 -0300

    Update take_screenshot.py

--- pro109/take_screenshot.py
+++ pro109/take_screenshot.py
@@ -33,7 +33,8 @@ while True:
                 x,y = int(lm_list[tip].x*w), int(lm_list[tip].y*h)
                 cv2.circle(img, (x,y), 15, (255, 0, 0), cv2.FILLED)
 
-                #escrevendo a condição para verificar se o dedo está dobrado, ou seja, verificar se o valor inicial da ponta do dedo é menor que a posição inicial do dedo que é o marco interno para o dedo indicador    
+                #escrevendo a condição para verificar se o dedo está dobrado, ou seja,
+                #verificar se o valor inicial da ponta do dedo é menor que a posição inicial do dedo que é o marco interno para o dedo indicador    
                 #se dedo estiver dobrado, mudar a cor para verde
                 if lm_list[tip].x > lm_list[tip - 3].x:
                     cv2.circle(img, (x,y), 15, (0, 255, 0), cv2.FILLED)
@@ -44,22 +45,22 @@ while True:
             print(finger_fold_status)
 
              #verificando se todos os dedos estão dobrados
-        if all(finger_fold_status):
+            if all(finger_fold_status):
                 
                 # ESCREVA O CÓDIGO AQUI  
              image = pyautogui.screenshot()
              image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
              cv2.imwrite("in_memory_to_disk.png", image)
-             pyautogui.screenshot("straight_to_disk.png")
+             pyautogui.screenshot("straight_to_disks.png")
              image = cv2.imread("straight_to_disk.png")
              cv2.imshow("Captura de tela", imutils.resize(image, width=600))
 
 
 
 
-         mp_draw.draw_landmarks(img, hand_landmark,
-         mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0,0,255),2,2),
-         mp_draw.DrawingSpec((0,255,0),4,2))
+            mp_draw.draw_landmarks(img, hand_landmark,
+            mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0,0,255),2,2),
+            mp_draw.DrawingSpec((0,255,0),4,2))
     
 
     cv2.imshow("Rastreamento de Maos", img)
